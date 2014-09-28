a4f1c21da919ec17a51901b068a08c79a1418f64
Kurt Bartholomew <kurt@kurtbartholomew.com>
2014-09-27 19:36:29 -0700

    Refactored static pages and migrated from sqllite to postgres

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,9 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
+
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
@@ -14,7 +17,7 @@ describe "Static pages" do
 		# Run the generator again with the --webrat flag if you want to use webrat methods/matchers
 		visit '/static_pages/home'
 		page.should have_selector('title', 
-						:text => "Ruby on Rails Tutorial Sample App | Home")
+						:text => "#{base_title} | Home")
     end
   end
 
@@ -28,7 +31,7 @@ describe "Static pages" do
   	it "should have the content 'Help'" do
   		visit '/static_pages/help'
   		page.should have_selector('title', 
-						:text => "Ruby on Rails Tutorial Sample App | Help")
+						:text => "#{base_title} | Help")
   	end
   end
 
@@ -42,8 +45,24 @@ describe "Static pages" do
   	it "should have the content 'About Us'" do
   		visit '/static_pages/about'
   		page.should have_selector('title', 
-					:text => "Ruby on Rails Tutorial Sample App | About Us")
+					:text => "#{base_title} | About Us")
   	end
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact Us'" do
+		# Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+		visit '/static_pages/contact'
+		page.should have_selector('h1', :text => 'Contact Us')
+    end
+
+    it "should have the content 'Sample App'" do
+		# Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+		visit '/static_pages/contact'
+		page.should have_selector('title', 
+						:text => "#{base_title} | Contact Us")
+    end
   end
 
 end
